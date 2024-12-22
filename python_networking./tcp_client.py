a06bd4f03465eb9a8352c546918b02595378fd64
zero cool. <alishbaabdulstealth@gmail.com>
2024-12-22 23:46:15 +0500

    Update tcp_client.py

--- python_networking./tcp_client.py
+++ python_networking./tcp_client.py
@@ -1,15 +1,11 @@
 import socket
 
-target_host = "127.0.0.1"  # use localhost for same machine.
-target_port = 9998         # Port number of the server
+target_host = "127.0.0.1"
+target_port = 9998
 
-# Create a socket object (TCP)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Connect to the server
 client.connect((target_host, target_port))
-# Send some data to the server
 client.send(b"Hello, Server!")
-# Receive a response from the server
 response = client.recv(4096)
 print(f"[*] Server response: {response.decode()}")
 client.close()
